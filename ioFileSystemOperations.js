61fc2024d02a585bff666d980d82a43c38179982
Christine L. Feaster <feasterc@gmail.com>
2016-02-22 22:36:02 -0800

    Complete exercise 3

---
+++ ioFileSystemOperations.js
@@ -0,0 +1,21 @@
+// MY FIRST I/O! (Exercise 3 of 13)
+
+/* Write a program that uses a single synchronous filesystem operation to
+ * read a file and print the number of newlines (\n) it contains to the
+ * console (stdout), similar to running cat file | wc -l.
+
+ * The full path to the file to read will be provided as the first
+ * command-line argument (i.e., process.argv[2]). You do not need to make
+ * your own test file.
+ */
+
+var fs = require('fs');
+var fileToParse = process.argv[2];
+
+function countNewlines(file) {
+    var contents = fs.readFileSync(file).toString();
+    return contents.split("\n").length - 1; 
+}
+
+console.log(countNewlines(fileToParse));
+
